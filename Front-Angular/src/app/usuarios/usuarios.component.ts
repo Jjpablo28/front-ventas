05625efde784ad06c9927ca5c3f9a1c6e50c288f
Jjpablo28 <jpr.rod06@gmail.com>
2025-11-13 16:36:59 -0500

    Usuarios ok

--- Front-Angular/src/app/usuarios/usuarios.component.ts
+++ Front-Angular/src/app/usuarios/usuarios.component.ts
@@ -11,9 +11,15 @@ export class UsuariosComponent implements OnInit {
   constructor(private usuariosService: UsuariosService) {
   }
 
+  ngOnInit(): void {
+    this.cargarUsuarios();
+  }
+
+
   nombreCompleto: any = '';
   correo: any = '';
   user: any = '';
+  eUser: any = '';
   contrasena: any = '';
   rol: any = '';
   estado: any = '';
@@ -23,7 +29,7 @@ export class UsuariosComponent implements OnInit {
   eRol: any = '';
   eEstado: any = '';
   usuarios: any[] = [];
-  userId:any='';
+  userId: any = '';
 
   show1 = false;
   show2 = false;
@@ -38,16 +44,35 @@ export class UsuariosComponent implements OnInit {
     this.show4 = false;
     this.show5 = false;
 
-    if (section === 1) this.show1 = true;
-    if (section === 2) this.show2 = true;
-    if (section === 3) this.show3 = true;
+    if (section === 1) {
+      this.rol = '';
+      this.correo = '';
+      this.nombreCompleto = '';
+      this.user = '';
+      this.userId = '';
+      this.show1 = true;
+    }
+    if (section === 2) {
+      this.rol = '';
+      this.correo = '';
+      this.nombreCompleto = '';
+      this.user = '';
+      this.userId = '';
+      this.show2 = true;
+    }
+    if (section === 3) {
+      if (this.nombreCompleto == '') {
+        alert('Debe seleccionar algún usuario');
+        return
+      }
+
+      this.show3 = true;
+      return
+    }
     if (section === 4) this.show4 = true;
-    if (section === 5) this.show5 = true;
+    //if (section === 5) this.show5 = true;
   }
 
-  ngOnInit(): void {
-    this.cargarUsuarios();
-  }
 
   cargarUsuarios() {
     this.usuariosService.getUser().subscribe({
@@ -78,6 +103,8 @@ export class UsuariosComponent implements OnInit {
     this.usuariosService.crearUser(body).subscribe({
       next: (data) => {
         console.log('Crear usuario');
+        this.cargarUsuarios();
+        this.toggle(1);
       },
       error: (err) => {
         console.log(err);
@@ -87,23 +114,67 @@ export class UsuariosComponent implements OnInit {
   }
 
   seleccionarEditar(usuario: any) {
-  this.usuarioSeleccionado=usuario
+
+
+    this.usuarioSeleccionado = usuario;
+    this.rol = this.usuarioSeleccionado.rol;
+    this.correo = this.usuarioSeleccionado.email;
+    this.nombreCompleto = this.usuarioSeleccionado.nombre_completo;
+    this.user = this.usuarioSeleccionado.username;
+    this.userId = this.usuarioSeleccionado.id;
+    this.toggle(3);
+    if (this.rol == 'ADMIN') {
+      this.rol = 'Administrador';
+    } else {
+      this.rol = 'Empleado';
+    }
   }
 
   actualizarUsuario() {
+    if (this.rol == 'Administrador') {
+      this.rol = 'ADMIN';
+    } else {
+      this.rol = 'USER';
+    }
+    const body = {
+      username: this.user,
+      email: this.correo,
+      rol: this.rol,
+      nombre_completo: this.nombreCompleto,
+    }
+    this.usuariosService.updateUser(this.userId, body).subscribe({
+      next: (data) => {
+        alert('Usuario actualizado!')
+        this.cargarUsuarios();
+        this.toggle(1)
+      },
+      error: (err) => {
+        console.log(err);
+        alert('Error al actualizar')
+      }
 
+    })
   }
 
   seleccionarEliminar(usuario: any) {
-    this.usuarioSeleccionado=usuario
-
-    this.userId=this.usuarioSeleccionado.id
-    console.log(this.usuarioSeleccionado.id)
+    this.usuarioSeleccionado = usuario
+    this.toggle(4);
+    this.userId = this.usuarioSeleccionado.id
 
   }
 
   eliminarUsuario() {
+    this.usuariosService.deleteUser(this.userId).subscribe({
+      next: (data) => {
+        alert('Usuario eliminado');
+        this.cargarUsuarios();
+        this.toggle(1);
 
+      },
+      error: (err) => {
+        alert('Error al eliminar')
+      }
+    })
   }
 
 
